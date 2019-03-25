609c2860351387dafa9a4467e72720d3d37161cd
Galena <850779090@qq.com>
2019-03-25 15:28:04 +0800

    2.0 commit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 * @Author: Galena
 * @Date:   2019-03-23 19:35:34
 * @Last Modified by:   Galena
-* @Last Modified time: 2019-03-23 22:03:34
+* @Last Modified time: 2019-03-25 15:15:34
 */
 var buttons = $('#buttonWrapper > div')
 var statusNow
@@ -15,7 +15,19 @@ function moveImage(number) {
 	$('.button').removeClass('buttonActive')
 	$(buttons[number]).addClass('buttonActive')
 }
+function setTime() {
+	time = window.setInterval(function(){
+		moveImage(n)
+		n = n+1
+		if(n===4){n=0}
+	},2000)
+}
 
+$('#box').on('mouseover', function() {
+	window.clearInterval(time)
+}).on('mouseout', function() {
+	setTime()
+})
 
 for (let i=0; i<buttons.length; i++) {
 	$(buttons[i]).on('click', function(e){
@@ -24,17 +36,64 @@ for (let i=0; i<buttons.length; i++) {
 	})
 }
 
-function setTime() {
-	time = window.setInterval(function(){
-		moveImage(n)
-		n = n+1
-		if(n===4){n=0}
-	},1000)
+
+//*****************************************************************
+var space = -960
+var mid = 4
+function moveImage2(direct) {
+	if (direct === 'right') {
+		mid = mid +1 
+		space = space - 960
+		$('#image2').css('transform','translateX('+space+'px)')
+		if (mid === 9) {
+			space = -960
+			window.setTimeout(function() {
+				$('#image2').removeClass('image2Toggle')
+				$('#image2').css('transform','translateX('+space+'px)')
+				window.setTimeout(function() {
+					$('#image2').addClass('image2Toggle')
+				},100)
+				// $('#image2').addClass('image2Toggle')
+			},500)
+			mid = 4
+		} 
+	} else if (direct === 'left') {
+		mid = mid - 1
+		space = space + 960
+		$('#image2').css('transform','translateX('+space+'px)')
+		if (mid === 3) {
+			space = -4800
+			window.setTimeout(function() {
+				$('#image2').removeClass('image2Toggle')
+				$('#image2').css('transform','translateX('+space+'px)')
+				window.setTimeout(function() {
+					$('#image2').addClass('image2Toggle')
+				},100)
+			},500)
+			mid = 8
+		}
+	}
 }
+function setTime2() {
+	time2 = window.setInterval(function(){
+		moveImage2('right')
+	},2000)
+}
+
+
+$('#rightButton').on('click', function() {
+	moveImage2('right')
+})
+$('#leftButton').on('click', function() {
+	moveImage2('left')
+})
+
+$('#box2').on('mouseover', function() {
+	window.clearInterval(time2)
+}).on('mouseout', function() {
+	setTime2()
+})
+
+//***************************************************
 setTime()
-$('#box').on('mouseover', function() {
-	window.clearInterval(time)
-})
-$('#box').on('mouseout', function() {
-	setTime()
-})
+setTime2()
